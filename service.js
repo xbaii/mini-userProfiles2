b5fb42dd46d3bafb71952df1938ab63df9c5cdfc
Bailey Leising <bleising10@gmail.com>
2015-01-21 11:54:31 -0700

    finished

--- service.js
+++ service.js
@@ -5,8 +5,8 @@ app.service('mainService', function($http) {
 
   this.getUsers = function() {
     return $http({
-        method: 'GET',
-        url: 'http://reqr.es/api/users?page=3'
-    })
+    	method: 'GET',
+    	url: 'http://reqr.es/api/users?page=1'
+    });
   }
 });
